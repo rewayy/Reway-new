2006496cdb17d2ee06be4ebc34ce122354b52b62
Khushi Sharma <96431469+khsh13@users.noreply.github.com>
2023-12-13 21:48:18 +0530

    Update rocket.js

--- src/rocket.js
+++ src/rocket.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import '../src/rocket.css'; 
+import '../src/rocket.css';
 
 function NewComponent() {
   const gifUrl = 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*TP2EU8KyIxct_-zTfnG4WQ.gif'; // Replace with the direct URL of your GIF
 
   return (
-    <div className="new-component-container">
-      <div className="content-container">
-        <div className="text-container">
-          <p className="rocket-txt">
-            At Reway, innovation thrives every day, breathing life into fresh ideas.
-          </p>
-          <p className="rocket-txt-small">
-          Our team continuously explores new horizons, pushing boundaries to create a sustainable and inclusive impact in the E-Waste Management ecosystem
-          </p>
-          <img src={gifUrl} alt="Rocket GIF" className="rocket-gif" />
-          <a className='rocket-txt-blog'>
-            To know more <br />
-            read our blogs.
-          </a>
+    <section name="rocket" id="home">
+      <div className="new-component-container">
+        <div className="content-container">
+          <div className="text-container">
+            <p className="rocket-txt">
+              At Reway, innovation thrives every day, breathing life into fresh ideas.
+            </p>
+            <p className="rocket-txt-small">
+              Our team continuously explores new horizons, pushing boundaries to create a sustainable and inclusive impact in the E-Waste Management ecosystem
+            </p>
+            <img src={gifUrl} alt="Rocket GIF" className="rocket-gif" />
+            <a className='rocket-txt-blog'>
+              To know more <br />
+              read our blogs.
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
